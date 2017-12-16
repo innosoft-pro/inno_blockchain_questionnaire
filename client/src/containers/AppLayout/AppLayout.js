a7b52453f566122164330808f750a586b5b8ae91
iplus <ivan.plyusnin@gmail.com>
2017-12-16 18:34:17 +0300

    add routes

--- client/src/containers/AppLayout/AppLayout.js
+++ client/src/containers/AppLayout/AppLayout.js
@@ -1,19 +1,17 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './AppLayout.css';
 import {Layout} from 'antd';
-import IndexPage from './containers/IndexPage/IndexPage';
 
 const {Header, Footer, Sider, Content} = Layout;
 
-class App extends Component {
+class AppLayout extends Component {
   render() {
     return (
       <div className="App">
         <Layout className="Layout">
           <Header>Header</Header>
           <Content className="Content">
-            <IndexPage/>
+            {this.props.children}
           </Content>
           <Footer className="Footer">Footer</Footer>
         </Layout>
@@ -22,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default AppLayout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppLayout from "./containers/AppLayout/AppLayout";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import IndexPage from "./containers/IndexPage/IndexPage";
+import LoginPage from "./containers/LoginPage/LoginPage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <AppLayout>
+          <PrivateRoute exact path="/" component={IndexPage} />
+          <Route path="/login" component={LoginPage} />
+        </AppLayout>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
 registerServiceWorker();
