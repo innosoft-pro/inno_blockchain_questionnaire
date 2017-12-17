df1aa1f0154661df0086ca94482904b22a8dba71
iplus <ivan.plyusnin@gmail.com>
2017-12-17 13:16:37 +0300

    add naming

--- client/src/containers/AppLayout/AppLayout.js
+++ client/src/containers/AppLayout/AppLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AppLayout.css';
-import {Layout} from 'antd';
+import {Layout, Icon} from 'antd';
 import {decl} from 'bem-react-core';
 const {Header, Footer, Content} = Layout;
 
@@ -10,11 +10,11 @@ export default decl({
   content() {
     return (
       <Layout className="Layout">
-        <Header>Header</Header>
+        <Header className="Header"><a href="/"><Icon type="aliwangwang-o" className="Logo" /> Le`Questionnaire</a></Header>
         <Content className="Content">
           {this.props.children}
         </Content>
-        <Footer className="Footer">Footer</Footer>
+        <Footer className="Footer"><a target="_blank" href="http://innosoft.pro">©&nbsp;Innosoft, 2017</a></Footer>
       </Layout>
     );
   }
