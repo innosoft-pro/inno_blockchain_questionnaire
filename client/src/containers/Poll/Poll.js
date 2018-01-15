606880ed4bdf20ecd2acb56fd64cfb13225f53ef
iplus <ivan.plyusnin@gmail.com>
2018-01-15 19:41:49 +0300

    show/download answers

--- client/src/containers/Poll/Poll.js
+++ client/src/containers/Poll/Poll.js
@@ -10,12 +10,17 @@ export default decl({
         this.onSubmit = this
             .onSubmit
             .bind(this);
+        this.loadAnswers = this.loadAnswers.bind(this);
     },
 
     content({match, store}) {
         const id = match.params.pollId;
         const poll = store.getById(id);
-        return <PollForm poll={poll} onSubmit={this.onSubmit}/>;
+        return <PollForm poll={poll} onLoadAnswers={this.loadAnswers} onSubmit={this.onSubmit}/>;
+    },
+
+    loadAnswers(id) {
+        this.props.store.loadAnswers(id);
     },
 
     onSubmit(values) {
