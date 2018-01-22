f3019e3f3a2b42219cd7170251b17069d5781321
Arthur Tazhitdinov <osteotek@gmail.com>
2018-01-22 23:25:55 +0300

    cleaning up

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-#docker build --tag "bchquiz_web:latest" -f ./srv/Dockerfile .
-#docker build --tag "bchquiz_bot:latest" -f ./bot/Dockerfile .
-#docker-compose -f "docker-compose.yml" -p "innoquiz" up
 docker-compose -p innoquiz up
